c0957d0d169c145589027018f51d82650aecda1d
Danick <45609733+RestartFU@users.noreply.github.com>
2022-01-31 22:13:21 -0500

    test

--- handler/punch_air.go
+++
@@ -1,8 +0,0 @@
-package handler
-
-import "github.com/df-mc/dragonfly/server/event"
-
-func (h *PlayerHandler) HandlePunchAir(ctx *event.Context) {
-	h.p.AddCPS(1)
-	h.p.Player().SendTip(h.p.CPS())
-}
